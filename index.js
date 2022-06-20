e83053e4b354de037a83511f5d41f819f0e9343f
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-06-20 01:27:19 +0100

    Extended the port from being hardcorded

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hurray am tying to master these backend"));
+app.get("/", (req, res) => res.send("Hello world"));
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => console.log("Listening on port 4000"));
+app.listen(port, () =>
+  console.log(`Server currently running at http://localhost:${port}`)
+);
